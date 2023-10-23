e759e150b35ae18ed9153809892378c053b0fcac
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-10-23 08:15:26 -0500

    updated index.js and seed.sql files

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const inquirer = require('inquirer');
-const connection = require('./db/connection');
+// const connection = require('./db/connection');
 
+// TODO: Create an array of questions for user input
+const questions = [{
+    type:'list' ,
+    name: 'option' ,
+    message: 'What would you like to do? ',
+    choices: [
+        'View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit'
+    ]
+}]
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((answers)=> {
+        console.log(answers);
+        switch (answers.option) {
+         case 0: 
+         option = 'View all departments';
+         break;
+         case 1: 
+         option = 'View all roles';
+         break
+        }
+    })
+} 
+init();
